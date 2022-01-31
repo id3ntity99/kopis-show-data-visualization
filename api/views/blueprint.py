8d2a003e6933af91f1e831ca216088ed85976a58
id3ntity99 <jas06113@gmail.com>
2022-01-31 23:47:14 +0900

    feat: view function에 logger 적용

--- api/views/blueprint.py
+++ api/views/blueprint.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from modules.request_api import Converter
 from modules.extract_data import GenreExtractor
 from modules.extract_data import DayExtractor
+from utils.logger.logger import Logger
 from redis_cache.redis_cache import RedisCache
 from flask import Blueprint
 from flask import request
@@ -13,6 +14,7 @@ blueprint = Blueprint("api", __name__, url_prefix="/api")
 load_dotenv(verbose=True)
 API_KEY_NAME = os.getenv("API_KEY_NAME")
 API_KEY_VAL = os.getenv("API_KEY_VALUE")
+logger = Logger()
 
 
 @blueprint.route("/genre", methods=["GET"])
@@ -30,13 +32,15 @@ def get_request_genre():
 
     if request.method == "GET":
         if cacher.does_exist():
-            print(f"[EXISTS] key : {REDIS_KEY}\n")
+            logger.set_new_format("%(asctime)s | %(name)s | %(levelname)s | EXSIST | %(message)s")
+            logger.log_caching("Cache exists")
             return cacher.get_redis()
         else:
             dict_data = requester.get_parsed_data()
             new_json = GenreExtractor(dict_data).get_new_json()
             time_out = datetime.timedelta(minutes=30)
             cacher.set_redis(new_json, time_out)
+            logger.log_caching("Saved in cache")
             return new_json
     else:
         return abort(400)
@@ -54,13 +58,15 @@ def get_request_day():
 
     if request.method == "GET":
         if cacher.does_exist():
-            print(f"[EXISTS] key : {REDIS_KEY}\n")
+            logger.set_new_format("%(asctime)s | %(name)s | %(levelname)s | EXSIST | %(message)s")
+            logger.log_caching("Cache exists")
             return cacher.get_redis()
         else:
             dict_data = requester.get_parsed_data()
             new_json = DayExtractor(dict_data).get_new_json()
             time_out = datetime.timedelta(minutes=60)
             cacher.set_redis(new_json, time_out)
+            logger.log_caching("Saved in cache")
             return new_json
     else:
         return abort(400)
