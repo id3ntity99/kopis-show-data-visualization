411b69139b212411df101baeac689167576b8ded
id3ntity99 <jas06113@gmail.com>
2022-01-31 23:45:44 +0900

    feat: 로그 포맷 변경 메서드 추가

--- api/utils/logger/logger.py
+++ api/utils/logger/logger.py
@@ -1,10 +1,27 @@
 import logging
 
+
+class Logger(object):
+    def __init__(self):
+        self.logger = logging.getLogger("cache_logger")
+        self.logger.setLevel(logging.DEBUG)
+        self.handler = logging.StreamHandler()
+        self.handler.setLevel(logging.DEBUG)
+        default_fmt = logging.Formatter(
+            fmt="%(asctime)s| %(name)s | %(levelname)s | %(message)s",
+            datefmt="%Y/%m/%d %I:%M:%S %p",
+        )
+        self.handler.setFormatter(default_fmt)
+        self.logger.addHandler(self.handler)
+
+    def set_new_format(self, new_fmt):
+        new_formatter = logging.Formatter(new_fmt, datefmt="%Y/%m/%d %I:%M:%S %p")
+        self.handler.setFormatter(new_formatter)
+
+    def log_caching(self, msg):
+        self.logger.info(msg)
+
+
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(message)s",
-        datefmt="%m/%d/%Y %I:%M:%S %p",
-    )
-    logging.info("I told you so!")
-    logging.warning("This is fake warning log")
+    logger = Logger()
+    logger.log_caching("Hello")
